22efd4b849c8df05aa70eed04722703c9116b645
Elias Svoba <elisvobs@live.com>
2019-12-05 12:24:08 +0000

    Making it better

--- app/src/main/java/com/elisvobs/skillar/MainActivity.java
+++ app/src/main/java/com/elisvobs/skillar/MainActivity.java
@@ -17,11 +17,41 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         int id = view.getId();
         switch (id) {
-            case R.id.skils:
-//                startActivity(new Intent(this, Skills.class));
+            case R.id.skills:
+                startActivity(new Intent(this, Skills.class));
                 break;
             case R.id.fin:
 //                startActivity(new Intent(this, Financial.class));
+                break;
+            case R.id.positive:
+//                startActivity(new Intent(this, Positive.class));
+                break;
+            case R.id.healthy:
+//                startActivity(new Intent(this, Healthy.class));
+                break;
+            case R.id.learner:
+//                startActivity(new Intent(this, Learner.class));
+                break;
+            case R.id.advisor:
+//                startActivity(new Intent(this, Advisor.class));
+                break;
+            case R.id.wiser:
+//                startActivity(new Intent(this, Wiser.class));
+                break;
+            case R.id.volunteer:
+//                startActivity(new Intent(this, Volunteer.class));
+                break;
+            case R.id.groomed:
+//                startActivity(new Intent(this, Groomed.class));
+                break;
+            case R.id.mentor:
+//                startActivity(new Intent(this, Mentor.class));
+                break;
+            case R.id.trained:
+//                startActivity(new Intent(this, Trained.class));
+                break;
+            case R.id.card:
+//                startActivity(new Intent(this, Card.class));
                 break;
         }
     }
