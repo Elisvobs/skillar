4b3fed24e45ab7e719bc6c311154983f4332cccd
Elias Svoba <elisvobs@live.com>
2020-07-15 23:32:28 +0000

    Making it better

---
+++ README.md
@@ -0,0 +1,52 @@
+# Skillar
+ **Version 1.0.0**
+ An android application crafted for the Elisvobs Foundation to equip you with essential life skills.
+
+## Android Libraries
+
+- Codecrafters Tableview
+- Recyclerview
+- SharePreferences
+
+## App Description
+
+- Display life skills you can acquire 
+- Includes information on healthy lifestyle 
+- Provide information on life changing techniques you can implement to have an impact in the society  
+
+## Screenshot
+<img src="" width="300"/>
+<img src="" width="300"/>
+
+## Contributors
+- Elias Svoba <elias@infixustech.co.zw>
+
+
+## License & Copyright
+
+Copyright (c) 2020 Infixus Tech
+
+Me, the author of the project, allow you to check the code as a reference.
+Beside the above, the following license applies and this license notice 
+must be included in all works derived from this project.
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
--- app/src/main/java/com/elisvobs/skillar/SkillsActivity.java
+++ app/src/main/java/com/elisvobs/skillar/SkillsActivity.java
@@ -1,8 +1,12 @@
 package com.elisvobs.skillar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.elisvobs.skillar.adapter.SkillsAdapter;
 import com.elisvobs.skillar.model.Skill;
 import com.elisvobs.skillar.util.DataManager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class SkillsActivity extends AppCompatActivity {
     private SkillsAdapter skillsAdapter;
+    private boolean isFabOpen = false;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
+    private FloatingActionButton fab, fab1, fab2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,15 @@ public class SkillsActivity extends AppCompatActivity {
         setTitle(R.string.skillz);
 
         skills();
+
+        fab = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
     }
 
     private void skills() {
@@ -45,4 +62,47 @@ public class SkillsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.fab:
+                animateFab();
+                break;
+            case R.id.fab1:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.fab2:
+                break;
+        }
+    }
+
+    private void animateFab() {
+        if (isFabOpen) {
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+
+            isFabOpen = false;
+        } else {
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+
+            isFabOpen = true;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        skillsAdapter.notifyDataSetChanged();
+    }
+
 }
